cd6cfe94fe7f842f4cc9e108291d81b1f211a127
Luke Martin <lukemartinwebdev@gmail.com>
2014-12-07 17:20:59 -0600

    Restructure assets called in header.php

--- includes/header.php
+++ includes/header.php
@@ -2,9 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <!--<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.css">
     <link href="/stylesheets/style.css" rel="stylesheet" type="text/css" />
+    <title><?php echo $page_title ?> | LiveList</title>
 </head>
  
 <body>
