27ee8eb066b8983801beb25dc752c10965998a1e
Luke Martin <lukemartinwebdev@gmail.com>
2014-12-12 23:36:05 -0600

    Reflect "name" to "username"

--- register.php
+++ register.php
@@ -1,4 +1,18 @@
+<?php require 'config/database.php';
+
+?>
+
+<?php $page_title = 'Register' ?>
 <?php include_once 'includes/header.php' ?>
+    <div class="span10 offset1">
+        <div class="row">
+            <h3>Register</h3>
+        </div>
+
+        <form class="form-horizontal" action="register.php" method="post">
+        	<input name="username" type="text"  placeholder="User Name" value="<?php echo !empty($username)?$username:'';?>">
+        </form>
+    </div>
 
 
 
