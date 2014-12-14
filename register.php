52e7ce8d45c47a050d153938cf4e013353fed41f
Luke Martin <lukemartinwebdev@gmail.com>
2014-12-13 18:49:46 -0600

    Add basic user registration

--- register.php
+++ register.php
@@ -1,4 +1,38 @@
-<?php require 'config/database.php';
+<?php 
+
+    require 'config/database.php';
+    if ( !empty($_POST)) {
+         // keep track validation errors
+        $usernameError = null;
+        $passwordError = null;
+         
+        // keep track post values
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+         
+        // validate input
+        $valid = true;
+        if (empty($username)) {
+            $nameError = 'Please enter User Name';
+            $valid = false;
+        }
+         
+        if (empty($password)) {
+            $emailError = 'Please enter Password';
+            $valid = false;
+        }
+         
+        // insert data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO users (username,password) values(?, ?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($username,$password));
+            Database::disconnect();
+            header("Location: index.php");
+        }
+    }
 
 ?>
 
@@ -14,10 +48,16 @@
         		   placeholder="User Name" 
         		   value="<?php echo !empty($username)?$username:'';?>"
             />
+            <?php if (!empty($usernameError)): ?>
+                <span class="help-inline"><?php echo $usernameError;?></span>
+            <?php endif; ?>
         	<input name="password" type="text"  
         		   placeholder="Password" 
         		   value="<?php echo !empty($password)?$password:'';?>"
             />
+            <?php if (!empty($passwordError)): ?>
+                <span class="help-inline"><?php echo $passwordError;?></span>
+            <?php endif;?>          
 	        <div class="form-actions">
 	          <button type="submit" class="btn btn-success">Join</button>
 	          <a class="btn btn-primary" href="index.php">Back</a>
